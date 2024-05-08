6a3a302906a0bcc8fc9fc3b06a0d66067f318189
Fedorov <phchen0769@icloud.com>
2024-05-08 22:10:34 +0800

    微调

--- src/config/index.ts
+++
@@ -1,1 +0,0 @@
-export const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:3003/" : "https://xxxx.com/";
--- src/enum.ts
+++ src/enum.ts
@@ -1,16 +1,9 @@
-// 提示框类型
-export enum DIALOG_TYPE {
-  INFO = "info",
-  ERROR = "error",
-  WARNING = "warning",
-  SUCCESS = "success" // 新增成功类型
-}
-// 消息code
+// 返回code
 export enum DATA_CODE {
   OK = 200,
   ERROR = 500
 }
-// rtc消息on
+// rtc on 消息
 export enum SOCKET_ON_RTC {
   // rtc 建立连接回调
   CANDIDATE = "candidate",
@@ -23,44 +16,23 @@ export enum SOCKET_ON_RTC {
   // 拒绝通话
   USER_REFUST = "user_refust"
 }
-// 系统消息on
+// 系统 on 消息
 export enum SOCKET_ON_SYS {
-  // 连接成功
-  CONNECTION = "connect",
-  // 连接失败
-  CONNECTION_ERROR = "connect_error",
+  // 连接socket
+  CONNECTION = "connection",
   // 断开socket
   DISCONNECT = "disconnect",
-  // 用户列表
-  USER_LIST = "user_list"
+  // 错误显示
+  SYS_ERROR = "sys_error"
 }
+// 用户on消息
 export enum SOCKET_EMIT {
   SYS_USER_LIST = "user_list"
 }
-// rtc协议状态
+// rtc连接消息类型
 export enum CALL_TYPE {
-  // 发起者
+  // 发送者
   SENDER = "sender",
   // 接收者
   RECIVER = "reciver"
-}
-// 通话状态
-export enum CALL_STATE {
-  // 等待
-  WAIT = 0,
-  // 发起通话中
-  SEND = 1,
-  // 连接成功
-  CONNECT = 2,
-  // 拒绝通话
-  REFUSE = -1,
-  // 挂断
-  OFF = -2
-}
-// 视频显示类型
-export enum SETTINGS_VIDEO {
-  // 共享屏幕
-  DISPLAY = "display",
-  // 视频通话
-  USER = "user"
 }
